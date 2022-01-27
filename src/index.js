05b6c8d2e1872562894af46d12f09c1cd8391425
Den4icGuti <goos7412@gmail.com>
2022-01-27 10:02:09 +0200

    add parameters fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './sass/main.scss';
+import API from './api/api-servise';
+import refsApi from './refs/refs-parameters';
+import Notiflix from 'notiflix';
+
+
+
+const refs = refsApi();
+
+refs.form.addEventListener('submit',onSearch)
+
+function onSearch(e) { 
+  e.preventDefault()
+
+  const searchQery = e.currentTarget.elements.searchQuery.value;
+  
+  if (searchQery.trim() === '') { 
+    Notiflix.Notify.info('Field must be filled')
+  }
+
+  API.fetchApi(searchQery).then(console.log)
+
+}
---
+++ src/refs/refs-parameters.js
@@ -0,0 +1,5 @@
+export default  function refsApi() { 
+  return {
+    form:document.querySelector('#search-form')
+  }
+}
